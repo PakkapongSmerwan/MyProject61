a967dcc81947e3d8b77aae53e0413b00b36e710f
apichat <apichat201@hotmail.com>
2018-12-03 17:30:54 +0700

    Phak

---
+++ VBStudyBIS3Q61P1/VBStudyBIS3Q61P1/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim name As String
+        Dim salary As Integer
+        name = MaskedTextBox1.Text
+        salary = MaskedTextBox2.Text
+        Label7.Text = name
+        Label8.Text = salary
+        Label9.Text = salary * 12
+
+
+    End Sub
+End Class
